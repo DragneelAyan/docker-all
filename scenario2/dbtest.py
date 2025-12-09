e4c5e272a2741091e759a518a1e175fca21565d7
DragneelAyan <ayandragneelsinha@gmail.com>
2025-12-09 21:22:14 +0530

    scenario2 complete

--- scenario2/dbtest.py
+++ scenario2/dbtest.py
@@ -2,7 +2,7 @@ import psycopg2
 print("Attempting connection...")
 
 conn = psycopg2.connect(
-    host="localhost",       # this will FAIL inside container
+    host="mydb",       # this will FAIL inside container
     port=5432,
     dbname="postgres",
     user="postgres",
